682cdcbd590fcca415156598cb062f1e5e784702
Stephen Daimler <stephen@packlate.com>
2011-05-25 19:55:19 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Helpatraveler::Application.config.secret_token = 'd57f8d0fac5b974b50a2e3a4c7d5f854fb9ada51e2869d989e487ed6fba31c86f672073a4a2e0e7f87bd74051ed7edd24c13ba9dac50ba7cd22df56f1864568b'
